7a40d0ecfd250a946d9b42d8f599eb464202e606
OusamSS <126404243+OusamSS@users.noreply.github.com>
2023-04-02 19:59:03 +0200

    Update sample.sql

--- sql/HR_analysis/sample.sql
+++ sql/HR_analysis/sample.sql
@@ -1,1 +1,11 @@
+# Find the number of employes each year grouped by gender
+SELECT
+	YEAR(hire_date) AS years,
+	gender,
+	COUNT(d.emp_no) AS num_emp
+FROM t_employees e 
+	JOIN t_dept_emp d ON e.emp_no = d.emp_no
+GROUP BY gender, years
+ORDER BY years, gender;
+
 
